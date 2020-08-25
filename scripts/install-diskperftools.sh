ca9278452d6d70febb322957af13f1a40eae3e06
Daniel Hibbert <dahibber@microsoft.com>
2020-08-25 16:12:51 -0400

    added anf attach

--- scripts/install-diskperftools.sh
+++ scripts/install-diskperftools.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Install PreReqs
 apt-get update
-
 apt-get install fio --yes
+apt-get install nfs-common --yes
+
+# attach NetAppFiles storage
+mkdir /mnt/anfPremium
+mkdir /mnt/anfStandard
+
+sudo mount -t nfs -o rw,hard,rsize=65536,wsize=65536,vers=3,tcp 10.200.200.4:/vol0 /mnt/anfPremium
+sudo mount -t nfs -o rw,hard,rsize=65536,wsize=65536,vers=3,tcp 10.200.200.4:/vol1 /mnt/anfStandard
